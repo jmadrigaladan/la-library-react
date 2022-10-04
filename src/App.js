59b5c9b8205ea0d74b191536138ce777820208f0
jmadrigaladan <jorgemadrigaladan@gmail.com>
2022-10-03 22:57:04 -0700

    working on cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import Nav from "./components/Nav.jsx";
+import Home from "./pages/Home.jsx";
+import Footer from "./components/Footer.jsx";
+import Books from "./pages/Books.jsx";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo.jsx";
+import Cart from "./pages/Cart.jsx";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    console.log(book, quantity);
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
   return (
-    <div className="App">
-      <Nav />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => (
+            <BookInfo books={books} addToCart={addToCart} cart={cart} />
+          )}
+        />
+        <Route
+          path="/cart"
+          render={() => (
+            <Cart books={books} cart={cart} changeQuantity={changeQuantity} />
+          )}
+        />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,29 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { library, Library } from "@fortawesome/fontawesome-svg-core";
-import { faBars, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-library.add(faBars, faShoppingCart)
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faShoppingCart,
+  faTimes,
+  faBolt,
+  faBookOpen,
+  faTags,
+  faStar,
+  faStarHalfAlt,
+  faArrowLeft,
+} from "@fortawesome/free-solid-svg-icons";
+library.add(
+  faBars,
+  faShoppingCart,
+  faTimes,
+  faBolt,
+  faBookOpen,
+  faTags,
+  faStar,
+  faStarHalfAlt,
+  faArrowLeft
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
